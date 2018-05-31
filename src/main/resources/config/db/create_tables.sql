d541addf0ba86ab858ce560eed08b384bb1f4c2a
edu <vizuete.eduardo@gmail.com>
2018-05-31 15:50:59 -0500

    country scripts and backup db

---
+++ src/main/resources/config/db/create_tables.sql
@@ -0,0 +1,27 @@
+-- Table: public.country
+
+-- DROP TABLE public.country;
+
+CREATE TABLE public.country
+(
+    id bigint NOT NULL,
+    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT pk_country PRIMARY KEY (id),
+    CONSTRAINT ux_country_name UNIQUE (name)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.country
+    OWNER to contractsappuser;
+
+-- Index: idx_country_name
+
+-- DROP INDEX public.idx_country_name;
+
+CREATE UNIQUE INDEX idx_country_name
+    ON public.country USING btree
+    (name COLLATE pg_catalog."default")
+    TABLESPACE pg_default;
